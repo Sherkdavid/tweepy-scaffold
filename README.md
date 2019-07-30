091d40e674ad61aee732fd4caa0a52780e0b3913
David Murphy <david.murphy@wandisco.com>
2019-07-30 08:22:55 +0100

    First Commit
    - Adds Twitter.py scaffold, README and requirements file

---
+++ README.md
@@ -0,0 +1,12 @@
+# Tweepy
+API Referernce at [Tweepy.org](http://docs.tweepy.org/en/v3.5.0/api.html)
+
+## Activate Twitter Developer Account
+
+Get your consumer and secret keys off [Twitter Dev Portal](http://dev.twitter.com),
+required to authenticate your application
+
+## Install Requirements
+Install tweepy library manually or via pip;
+
+`pip install -r requirements.txt`
---
+++ Twitter.py
@@ -0,0 +1,14 @@
+import tweepy
+
+def main():
+    # http://docs.tweepy.org/en/v3.5.0/api.html
+    # Login Account
+    auth = tweepy.auth.OAuthHandler("Your Consumer Key", "Your Consumer Secret")
+
+    # Validate application with dev tokens
+    auth.set_access_token("Your Dev Key", "Your Dev Secret")
+    api = tweepy.API(auth)
+
+    # Get Timeline
+    consumer_timeline = api.home_timeline()
+2
